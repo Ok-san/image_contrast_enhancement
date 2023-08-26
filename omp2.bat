5db2b6c9c72cd363b58b16a976c7864ae4b9b7cd
Ok-san <osyrova.work@gmail.com>
2023-08-27 01:51:44 +0300

    bat

---
+++ omp2.bat
@@ -0,0 +1,5 @@
+omp2.exe test_data/in.pnm test_data/out.pnm -1 0.00390625
+omp2.exe test_data/in.pnm test_data/out.pnm 2 0.00390625
+omp2.exe test_data/in.pnm test_data/out.pnm 4 0.00390625
+omp2.exe test_data/in.pnm test_data/out.pnm 0 0.00390625
+pause
